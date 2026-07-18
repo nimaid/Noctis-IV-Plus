44d934631c30666c541d063130340efa809690c0
Ella Jameson <ellagjameson@gmail.com>
2026-07-18 00:30:46 -0700

    Revamped compile script

--- source/compile.bat
+++ source/compile.bat
@@ -1,26 +1,49 @@
-rem @echo off
-set path=C:\bc.31\bin;C:\bc.31\INCLUDE
-rem set INCLUDE=C:\bc.31\INCLUDE
-if not exist bin mkdir bin
-cd bin
-rem del *.* /q
-cd ..
+@echo off
 
-if exist bin\noctis.exe del bin\noctis.exe
+rem Change this to point to the installation directory for Borland C++ 3.1
+rem This should be relative to the DOS C: drive, not your main C: drive
+set BCPP31_DIR=C:\bc.31
+
+
+
+echo Building Noctis IV Plus...
+
+rem Append Borland C++ 3.1 to the PATH
+set PATH=%PATH%;%BCPP31_DIR%\bin;%BCPP31_DIR%\INCLUDE
+
+rem Ensure the 'bin' folder exists and is empty
+if exist bin\NUL deltree /Y bin
+mkdir bin
+
+rem Build Noctis IV using the make tool
 make -fnoctis.mak -DALL=DEF_ALL -B
+
+rem Handle failure to build
 if exist bin\noctis.exe goto success
+echo Build failed!
 pause
 goto done
 
+rem Handle successful build
 :success
+echo Build completed successfully!
+
+rem Move built executable
+echo Moving built executable to modules folder...
 copy bin\noctis.exe ..\modules\noctis.exe /y
 copy supports.nct ..\data /y
+del bin\noctis.exe
+echo Executable moved successfully!
 
-rem del noctis.exe
-pause
-:done
+rem Pause before running
+echo Press any key to run Noctis IV Plus . . .
+pause >nul
 
+rem Run Noctis IV
 cd ..
 modules\noctis.exe
 cd ..
 cd source
+
+rem End of program
+:done
